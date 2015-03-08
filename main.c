96ecb26de354dcb4dba8d62951f95ab8d2c59e61
SergioRuedaRobles <serdrag@gmail.com>
2015-03-08 18:11:38 +0100

    MayorMenor1

---
+++ main.c
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.c
+ * Author: Sergio
+ *
+ * Created on 8 de marzo de 2015, 17:21
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void comparar(int num1, int num2){
+    if(num1>num2){
+        printf("El primero es mayor que el segundo");
+    }else if(num1<num2){
+        printf("El segundo es mayor que el primero");
+    }else{
+        printf("Son iguales");
+    }
+}
+int leerNum(){
+    int x; 
+    printf("Escribe un numero: ");
+    scanf("%d",&x);
+    printf("%d",x);
+    printf("%\n");
+    return x;
+}
+
+int main(int argc, char** argv) {
+    int x,y;
+    x=leerNum();
+    y=leerNum();
+    comparar(x,y);
+    return (EXIT_SUCCESS);
+}
+
